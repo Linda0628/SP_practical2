12aa258ccdfa7bd92f5fc3a9c41189866c0dc4b2
Linda0628 <91074814+Linda0628@users.noreply.github.com>
2021-10-21 23:17:28 +0100

    Add files via upload

---
+++ WG07_P2.R
@@ -0,0 +1,129 @@
+COVID19_MODEL <- function(  
+          # initial susceptible population size
+          N=5.5*1e6, 
+          # the total day
+          TIME_SPAN=100, 
+          # the number of initial exposed people
+          INIT_EXPOSED_NUM=10, 
+          # overall viral infectivity parameter
+          LAMBDA=0.4/N, 
+          #10% of the population with the lowest βi values
+          CAUTIOUS_RATE=0.1, 
+          #random sample of 0.1% of the population
+          SAMPLE_RARE=1e-3, 
+          # Once exposed they have a daily probability of 1/3 of entering the infectious state
+          E2I = 1/3, 
+          # The infectious have a daily probability of 1/5 of leaving the infectious state
+          # # (recovery or transition to serious disease)
+          I2R = 1/5) 
+
+{
+  # generate beta_i
+  BETA <- rlnorm(N, 0, 0.5)
+  BETA <- BETA/mean(BETA)
+  
+  ########## Records ##########
+  # new infections each day
+  new_nums <- rep(0, TIME_SPAN)
+  # the number of new infections among cautious people with the lowest beta_i values
+  new_nums_cautious  <- rep(0, TIME_SPAN)
+  # the number of new infections among sampled SAMPLE_RARE people
+  new_nums_sampled  <- rep(0, TIME_SPAN)
+  
+  # mark the top-10% cautious people
+  cautious_ids <- order(BETA,decreasing=FALSE)[1:(CAUTIOUS_RATE*N)]
+  
+  # mark 0.1% sample
+  sample_ids <- sample(1:N, N*SAMPLE_RARE, replace = F)
+  
+  # Start the epidemic by setting 10 randomly chosen people to the exposed (E) state
+  E_ids <- sample(1:N, INIT_EXPOSED_NUM)
+  
+  ## initialize to susceptible state
+  x <- rep(0,N) 
+  ## create some E state
+  x[E_ids] <- 1 
+  ## set up storage for pop in each state 
+  S <- E <- I <- R <- rep(0,TIME_SPAN) 
+  ## initialize
+  S[1] <- N-INIT_EXPOSED_NUM ; E[1] <- INIT_EXPOSED_NUM  
+  
+  for (i in 2:TIME_SPAN) {
+    sum_beta = sum(BETA[x==2])
+    infected_prob  <- rep(0,N)
+    infected_prob[which(x==0)] = LAMBDA * sum_beta * BETA[x==0]
+    u = runif(N)
+    # the index in the S stage (before) one day Infection process
+    index1 <- which(x==0) 
+    ## I -> R with prob delta
+    x[x==2&u<I2R] <- 3 
+    ## E -> I with prob gamma 
+    x[x==1&u<E2I] <- 2 
+    ## S -> E with prob beta*I[i-1]
+    x[x==0&u<infected_prob] <- 1
+    # the index in the S stage (after) one day Infection process
+    index2 <- which(x==0) 
+    # the index that from S stage to E stage (after) one day Infection process
+    index <- index1[index1 %in% index2 == F] 
+    # the number of people in each stage (after) one day Infection process
+    S[i] <- sum(x==0); E[i] <- sum(x==1)
+    I[i] <- sum(x==2); R[i] <- sum(x==3)
+    #(1) new whole population
+    new_nums[i] <- S[i-1] - S[i]
+    #(2) new cautious data (10%)
+    index_cautious = cautious_ids[cautious_ids %in% index]
+    new_nums_cautious[i] = length(index_cautious)
+    #(3) new random sample data (0.1%)
+    index_sample = sample_ids[sample_ids %in% index]
+    new_nums_sampled[i] = length(index_sample)
+  }
+  list(new_nums=new_nums,
+       new_nums_cautious=new_nums_cautious,
+       new_nums_sampled=new_nums_sampled)
+}
+
+par(mfrow=c(2,1),mar=c(2.7,2.7,2,1))
+i=1 ; runs = 2
+while(i<=runs){
+  res <- COVID19_MODEL() 
+  # standardized three different data
+  new_nums = scale(res$new_nums)
+  new_nums_cautious = scale(res$new_nums_cautious)
+  new_nums_sampled = scale(res$new_nums_sampled)
+  # The maximum of three different data
+  y_max=max(max(new_nums),max(new_nums_cautious),max(new_nums_sampled))
+  # plot new whole population distribution
+  plot(new_nums,ylim=c(-1,y_max),xlim=c(0,110)
+       type = 'l', lwd=2, col=1,
+       main = c('Standardized New Infections Daily')) 
+  # Add new cautious data (10%) distribution
+  lines(new_nums_cautious,col=2,lty=1,lwd=2) 
+  # Add new random sample data (0.1%) distribution
+  lines(new_nums_sampled,col=3,lty=1,lwd=2) 
+  # peaks for three different curves
+  abline(v=c(which.max(new_nums),
+             which.max(new_nums_cautious), 
+             which.max(new_nums_sampled)),
+         col=c(1,2,3),lty=c(2,2,2))
+  abline(h= c(max(new_nums),
+              max(new_nums_cautious),
+              max(new_nums_sampled)),
+         col=c(1,2,3),lty=c(2,2,2))
+  # mark the peaks' coordinate in the plot
+  text(c(which.max(new_nums),
+         which.max(new_nums_cautious),
+         which.max(new_nums_sampled)),
+       c(max(new_nums),
+         max(new_nums_cautious),
+         max(new_nums_sampled)),
+       c(paste("(" , which.max(new_nums) , "," , max(new_nums) , ")"),
+         paste("(" , which.max(new_nums_cautious) , "," , max(new_nums_cautious) , ")"),
+         paste("(" , which.max(new_nums_sampled) , "," , max(new_nums_sampled) , ")")),
+       col=c(1,2,3),cex=c(0.7,0.7,0.7))
+  legend('center',c("whole population","cautious 10%","0.1% random sample"),
+         col=c(1,2,3),bty='n',lty=c(1,1,1))
+  i=i+1
+}
+
+
+
